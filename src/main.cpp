f99a7a17a19864b253736b57c5c4a8186e6bdbb6
jc9677 <jeff.campagnola@gmail.com>
2022-11-07 16:58:30 -0500

    Pot input, scaled to +/-

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,81 @@
 #include <Arduino.h>
-int Distance = 0;  // Record the number of steps we've taken
+#include <AccelStepper.h>
 
-void setup() {               
-  pinMode(26, OUTPUT);    
-  pinMode(27, OUTPUT);
-  digitalWrite(26, LOW);
-  digitalWrite(27, LOW);
+// Define the stepper and the pins it will use
+AccelStepper stepper1(AccelStepper::DRIVER, 27, 26);
+
+// Define our three input button pins
+#define  LEFT_PIN  4
+#define  STOP_PIN  3
+#define  RIGHT_PIN 2
+
+// Define our analog pot input pin
+#define  SPEED_PIN 0
+
+// Define our maximum and minimum speed in steps per second (scale pot to these)
+#define  MAX_SPEED 3600
+#define  MIN_SPEED 0.1
+
+void setup() {
+  analogReadResolution(10);
+  Serial.begin(115200);
+  // The only AccelStepper value we have to set here is the max speeed, which is higher than we'll ever go
+  stepper1.setMaxSpeed(10000.0);
+ 
+  // Set up the three button inputs, with pullups
+  pinMode(LEFT_PIN, INPUT_PULLUP);
+  pinMode(STOP_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(27, HIGH);
-  delayMicroseconds(75);         
-  digitalWrite(27, LOW);
-  delayMicroseconds(75);
-  Distance = Distance + 1;   // record this step
+  static float current_speed = 0.0;         // Holds current motor speed in steps/second
+  static int analog_read_counter = 1000;    // Counts down to 0 to fire analog read
+  static char sign = 0;                     // Holds -1, 1 or 0 to turn the motor on/off and control direction
+  static int analog_value = 0;              // Holds raw analog value.
+  
+
  
-  // Check to see if we are at the end of our move
-  if (Distance == 5376)
-  {
-    // We are! Reverse direction (invert DIR signal)
-    //if (digitalRead(26) == LOW)
-    //{
-    //  digitalWrite(26, HIGH);
-    //}
-    //else
-    //{
-    //  digitalWrite(26, LOW);
-    //}
-    // Reset our distance back to zero since we're
-    // starting a new move
-    Distance = 0;
-    // Now pause for half a second
-    delay(300);
+  // If a switch is pushed down (low), set the sign value appropriately
+/*   if (digitalRead(LEFT_PIN) == 0) {
+    sign = 1;
   }
+  else if (digitalRead(RIGHT_PIN) == 0) {    
+    sign = -1;
+  }
+  else if (digitalRead(STOP_PIN) == 0) {
+    sign = 0;
+  }
+ */
+  // We only want to read the pot every so often (because it takes a long time we don't
+  // want to do it every time through the main loop).  
+  if (analog_read_counter > 0) {
+    analog_read_counter--;
+  }
+  else {
+    analog_read_counter = 300;
+    // Now read the pot (from 0 to 1023)
+    analog_value = analogRead(SPEED_PIN);
+    Serial.print("Analog: ");
+    Serial.print(analog_value);
+    // Give the stepper a chance to step if it needs to
+    stepper1.runSpeed();
+    //  And scale the pot's value from min to max speeds
+    if (analog_value > 515) {
+      sign = 1;
+    }
+    if (analog_value < 509) {
+      sign = -1;
+    }
+      
+    current_speed = ((((analog_value-512)/512.0) * (MAX_SPEED - MIN_SPEED)) + MIN_SPEED);
+    Serial.print("    Scaled: ");
+    Serial.print(current_speed);
+    Serial.println("");
+    // Update the stepper to run at this new speed
+    stepper1.setSpeed(current_speed);
+  }
+
+  // This will run the stepper at a constant speed
+  stepper1.runSpeed();
 }
